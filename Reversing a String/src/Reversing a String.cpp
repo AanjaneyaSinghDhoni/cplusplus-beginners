fde54a1fe71ba90cd0011484303bb6273dcee57f
Aanjaneya <2016ccaanjaneya4789@poornima.edu.in>
2018-01-15 23:49:41 +0530

    added comments
    added comments in reversal of string program

--- Reversing a String/src/Reversing a String.cpp
+++ Reversing a String/src/Reversing a String.cpp
@@ -10,14 +10,20 @@
 using namespace std;
 
 int main() {
-
+	
+	//Entering the string 
 	char text[] = "Hello there!";
-
+	
+	//calculating the size of text
 	int nChars = sizeof(text)-1;
-
+	
+	//Assiging the address of text array to a pointer 
 	char *pStart = text;
-	char *pEnd = text + nChars - 1;
 
+	//setting the last alphabet adrress to a pointer
+ 	char *pEnd = text + nChars - 1;
+
+	//Swapping the string into reverse string
 	while(pStart < pEnd) {
 
 		char save = *pStart;
@@ -28,7 +34,8 @@ int main() {
 		pEnd--;
 	}
 
-	cout << text << endl;
+	//printing reverse string
+	cout <<"\n"<<text<< endl;
 
 
 	return 0;
